076f01e782d53fcf208ced9899864a3ec4887e72
unknown <ivandiazjvj@gmail.com>
2021-09-23 18:43:15 -0300

    Mercado Liebre

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static('public'));
-const puerto = 3002;
+const PORT = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve('./views/home.html'))
 })
 
 
-app.listen(puerto,()=>{
+app.listen(PORT,()=>{
     console.log('El server esta corriendo')
 }) 
