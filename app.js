c3a697c32ac5237aa9d2bc9679908f9843f879b9
unknown <ivandiazjvj@gmail.com>
2021-09-23 18:55:46 -0300

    s

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve('./views/home.html'))
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
 
 
